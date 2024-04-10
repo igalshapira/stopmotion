facf557ee937302c040840da927051ded7a6ded4
Igal Shapira <igshapir@microsoft.com>
2024-04-09 21:08:40 -0700

    Prompt 1

---
+++ README.md
@@ -0,0 +1,12 @@
+<!-- README.md -->
+# Webcam Capture
+
+This is a simple static web application that uses the webcam to capture images. It is hosted on GitHub Pages.
+
+## Usage
+
+Open the web page, allow access to your webcam, and click the "Capture" button to take a picture. You can also press the "space" key as a shortcut.
+
+This project was created based on the following prompt:
+
+"Generate a project to be hosted in Github. The project will be a static web application, hosted in Github pages. The application will use the webcam to capture images. Add "Capture" button that will take a single picture from the web cam. Use "space" key as shortcut for it. Add a README file which describes the project and also share the prompt that was used to create it."
---
+++ main.js
@@ -0,0 +1,31 @@
+// main.js
+window.addEventListener('DOMContentLoaded', (event) => {
+    const video = document.getElementById('webcam');
+    const captureButton = document.getElementById('capture');
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+            })
+            .catch(function (error) {
+                console.log("Something went wrong!");
+            });
+    }
+
+    captureButton.addEventListener('click', function() {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        context.drawImage(video, 0, 0);
+    });
+
+    window.addEventListener('keydown', function(event) {
+        if (event.key === ' ') {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            context.drawImage(video, 0, 0);
+        }
+    });
+});
